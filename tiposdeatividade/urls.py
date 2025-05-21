8f9e1d08cfc76ef32bb2998da33d2d06a1fe4002
BarrosJu <eujulianabarros018@gmail.com>
2025-05-21 19:26:38 -0300

    django 2

--- tiposdeatividade/urls.py
+++ tiposdeatividade/urls.py
@@ -5,12 +5,12 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("exibemensagem", views.exibe_mensagem, name="exibe_mensagem"),
-<<<<<<< HEAD
+
     path("exibemensagemsimples", views.exibe_html_simples,
           name="exibe_mensagem_simples"),
     path("test_render",views.test_render, name="render_exibir"),
-=======
+
     path('exibemensagensimples', views.exibe_html_simples, name="exibe_html_simples"),
     path("test_render", views.test_render,name="render_exibir"),
->>>>>>> b531f7fa8ffbad8ef919f411a5883709e336a0a9
+
 ]
--- utilitario/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import HttpResponse, render
-
-# Create your views here.
-def exibe_mensagem(request):
-    t_html = '<html><body>Utilitario</body></html>'
-    return HttpResponse(t_html)
