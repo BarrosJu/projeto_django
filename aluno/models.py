71166ad86876b66843e633f172f044665d77b4d5
BarrosJu <eujulianabarros018@gmail.com>
2025-06-04 21:37:34 -0300

    ajuste models alunos

--- aluno/models.py
+++ aluno/models.py
@@ -5,9 +5,18 @@ from django.db import models
 class Aluno(models.Model):
     matricula = models.AutoField(primary_key=True,
                               help_text='Ente com o numero da matricula do aluno')
-    nome= models.CharField(max_length=100, help_text='Informe o nome do aluno')
-    data_incial = models.DateField('Informe a data de inicio do aluno')
-    data_final = models.DateField('Informe a data final do aluno')
+    
+    nome= models.CharField(max_length=100,
+                            help_text='Informe o nome do aluno')
+    
+    data_incial = models.DateField(null=False,
+                                   default=timezone.now(),
+                                   help_text='Informe a data de inicio do aluno')
+    
+    data_final = models.DateField(null=True,
+                                  blank=True,
+                                  default=timezone.now(),
+                                  help_text='Informe a data final do aluno')
 
     def __str__(self):
         return f'{self.matricula} - {self.nome} - {self.data_incial} - {self.data_final}'
